8b366ef2d1c61bf096c5e89b7ce485b9ffac65df
Ali Arab <aliarab812@yahoo.com>
2021-03-11 23:15:31 -0600

    date and time added

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+// resource: https://momentjs.com/
+// Display today's date and time
+var todayDate = $("#currentDay");
+todayDate.text(moment().format('dddd, MMMM Do YYYY, h:mm a'));
+
+
+
+
+// get local storage info setup
+
+
+// work on the time/hour
+
+// update colors according to user input
+
+// save the inserts 

097c20c0fd76a684b957880086f123d158a6c9ab
Ali Arab <aliarab812@yahoo.com>
2021-03-12 01:07:17 -0600

    formating html document

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,13 @@
 
 //WHEN I open the planner 
 //THEN the current day is displayed at the top of the calendar
+// https://momentjs.com
 var todayDate = $("#currentDay");
 todayDate.text(moment().format('dddd, MMMM Do YYYY, h:mm a'));
 
 // WHEN I scroll down
 // THEN I am presented with timeblocks for standard business hours
-// refer to lines 22-102 on the index.html
+// refer to lines 25-84 on the index.html
 
 // WHEN I view the timeblocks for that day 
 // THEN each timeblock is color coded to indicate whether it is in the past,  present, or future

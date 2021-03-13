ef9c0c826a534413edd26b730eb78498cf9b1a9e
Ali Arab <aliarab812@yahoo.com>
2021-03-12 21:34:44 -0600

    color block done in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,19 +6,33 @@
 var todayDate = $("#currentDay");
 todayDate.text(moment().format('dddd, MMMM Do YYYY, h:mm a'));
 
-// WHEN I scroll down
-// THEN I am presented with timeblocks for standard business hours
-// refer to lines 25-84 on the index.html
-
 // WHEN I view the timeblocks for that day 
 // THEN each timeblock is color coded to indicate whether it is in the past,  present, or future
+ var timeblock = $(".time-block");
+ console.log(timeblock);
 
-var dt = new Date();
-var hr = dt.getHours();
+ timeblock.each(function () { // Loop started
+    var currentEL = $(this);
+    console.log(currentEL.attr("id"));
 
-function checkPresent () {
+    var id = currentEL.attr("id");
+    console.log(id.replace("hour_", ""));
 
-}
+    id = id.replace("hour_", "");
+
+    var hr = moment().hour();
+    console.log(id, hr);
+
+    if (id < hr) {
+        currentEL.addClass("past");
+    } else if (id == hour) {
+        currentEL.addClass("present");
+    } else {
+        currentEL.addClass("future");
+    }
+ })
+
+
 
 // add time blocks and make sure to color code them
 // how to check whether it'll be past,present or future

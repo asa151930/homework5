7ac21af6f808a3fd9cb7c2eb20fe6ec7813d592b
Ali Arab <aliarab812@yahoo.com>
2021-03-13 15:34:27 -0600

    html military time updated with js savebtn codes

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,47 +3,77 @@
 //WHEN I open the planner 
 //THEN the current day is displayed at the top of the calendar
 // https://momentjs.com
-var todayDate = $("#currentDay");
-todayDate.text(moment().format('dddd, MMMM Do YYYY, h:mm a'));
 
-// WHEN I view the timeblocks for that day 
-// THEN each timeblock is color coded to indicate whether it is in the past,  present, or future
-var timeblock = $(".time-block");
-console.log(timeblock);
+$(document).ready(function () {         // when the document is ready, set the click event on 
 
-timeblock.each(function () { // Loop started
-    var currentEL = $(this);
-    console.log(currentEL.attr("id"));
+    var todayDate = $("#currentDay");
+    todayDate.text(moment().format('dddd, MMMM Do YYYY, h:mm a'));
 
-    var id = currentEL.attr("id");
-    console.log(id.replace("hour_", ""));
+    // WHEN I view the timeblocks for that day 
+    // THEN each timeblock is color coded to indicate whether it is in the past,  present, or future
+    var timeblock = $(".time-block");
+    console.log(timeblock);
 
-    id = id.replace("hour_", "");       // replaces the string with the number
+    timeblock.each(function () { // Loop started
+        var currentEL = $(this);
+        console.log(currentEL.attr("id"));
 
-    var hr = moment().hour(); // log in hours
-    console.log(id, hr);
+        var id = currentEL.attr("id");
+        console.log(id.replace("hour_", ""));
 
-    if (id < hr) {          // timeblock gets color coded with past, present, and future
-        currentEL.addClass("past");
-    } else if (id == hour) {
-        currentEL.addClass("present");
-    } else {
-        currentEL.addClass("future");
-    }
-})
+        id = id.replace("hour_", "");       // replaces the string with the number
 
-// WHEN I click into a timeblock 
-// THEN I can enter an event 
-// WHEN I click the save button for that timeblock 
-// THEN the text for that event is saved in local storage 
-var saveBtn = $(".saveBtn");
-console.log(saveBtn);
+        var hr = moment().hour(); // log in hours
+        console.log(id, hr);
 
-$(document).ready(function () {            // when the document is ready, set the click event on 
-    
-    $('.saveBtn').on('click', function () {       // function give us to do something after the click
-       
+        console.log(localStorage.getItem(id))
+
+        var existingTask = localStorage.getItem(id);
+
+        if (existingTask) {
+            currentEL.children('textarea').val(existingTask);
+        }
+
+        if (id < hr) {          // timeblock gets color coded with past, present, and future
+            currentEL.addClass("past");
+        } else if (id == hr) {
+            currentEL.addClass("present");
+        } else {
+            currentEL.addClass("future");
+        }
+    })
+
+    // WHEN I click into a timeblock 
+    // THEN I can enter an event 
+    // WHEN I click the save button for that timeblock 
+    // THEN the text for that event is saved in local storage 
+    var saveBtn = $(".saveBtn");
+    console.log(saveBtn);
+
+    saveBtn.on('click', function (event) {
+        event.preventDefault();
+        // function give us to do something after the click
+        var text = $(this).prev().val();
+        console.log(text);
+        var hour = $(this).parent();
+        console.log(hour);
+
+        var hour = $(this).parent().attr('id').replace("hour_", "");
+        console.log(hour);
+        // save to local storage
+        
+
+        if (text !== "") { 
+            localStorage.setItem(hour, text);
+        } else {
+            localStorage.removeItem(hour)
+        }
+
     });
 });
+
+
+
+
 
 
